971d3e2e442c067aad9625927ad84310347aaaf5
Tautvydas <tautvydas@dulskis.eu>
2018-11-22 18:39:10 +0200

    8 paskaita

--- js/vanila.js
+++ js/vanila.js
@@ -1,26 +1,40 @@
-function sudeti() {
-    var a = gauti('A');
-    var b = gauti('B');
-    // Atliekam operacijas
-    rodytiAtsakyma(rez);
-}
-var xx = '0';
-function atimti() {
-    var a = gauti('A');
-    var b = gauti('B');
-    // Atliekam operacijas
-    rodytiAtsakyma(rez);
-}
+let klase1 = [
+    {vardas: "Jonas", pavarde: "Jonaitis", amzius: 34},
+    {vardas: "Petras", pavarde: "Petraitis", amzius: 56},
+    {vardas: "Zigmas", pavarde: "Zigmaitis", amzius: 12},
+    {vardas: "Ona", pavarde: "Onaityte", amzius: 45},
+    {vardas: "Zita", pavarde: "Zitiene", amzius: 65},
+    {vardas: "Rita", pavarde: "Rutele", amzius: 21}
+];
 
-function gautiAtsTeksta() {
-    if (xx === 0) {
-        xx = document.getElementById('rezultatas').innerHTML;
+let klase2 = [
+    {vardas: "Jonas", pavarde: "ppp", amzius: 12},
+    {vardas: "Petras", pavarde: "ppp", amzius: 12},
+    {vardas: "Zigmas", pavarde: "ppp", amzius: 12},
+    {vardas: "Ona", pavarde: "ppp", amzius: 12},
+    {vardas: "Zita", pavarde: "ppp", amzius: 12},
+    {vardas: "Rita", pavarde: "ppp", amzius: 12}
+];
+
+let spausdinti = function (kur, ka) {
+    kur.innerHTML += ka;
+};
+
+function formuotiSarasa(ka) {
+    let sarasas = '';
+    for (let i = 0; i < ka.length; i++) {
+        let asmuo = ka[i];
+        sarasas += asmuo.vardas + " " + asmuo.pavarde + " amzius: " + asmuo.amzius + "<br/>";
     }
 
-    return xx;
+    return sarasas;
 }
-/** *
- *
- * var kint = document.getElementById().value;
- *
- */
+
+let sarasas1 = formuotiSarasa(klase1);
+let sarasas2 = formuotiSarasa(klase2);
+
+let kur1 = document.getElementsByClassName('testas')[0];
+let kur2 = document.getElementById('testas');
+
+spausdinti(kur1, sarasas1);
+spausdinti(kur2, sarasas2);
